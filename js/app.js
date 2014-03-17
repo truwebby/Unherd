90fcf74178831c93cc3a10aa5af71958834dd9e4
unknown <truwebby@gmail.com>
2014-03-17 19:54:33 +0530

    favicon added and UI fixes

--- js/app.js
+++ js/app.js
@@ -6,17 +6,13 @@ $(document).foundation();
 
 // Initializing masonry
 
-var container = document.querySelector('#mainSection');
-var msnry = new Masonry( container, {
-  // options
-//  columnWidth: 200,
-  itemSelector: 'optimize'
-});
 
-var $container = $('#work_grid');
 
-//$container.imagesLoaded( function(){
-// $('#work_grid').masonry({
-//  itemSelector: '.work_item',
-//  isAnimated: true,
-//});
+var $root = $('html, body');
+    $('#scrollUp').click(function() {
+        $root.animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 700);
+        return false;
+
+    });
